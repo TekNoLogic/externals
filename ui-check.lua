7a66713bec260945a5e2ee6eadcc0127db0278d5
tekkub :bear:  <tekkub@gmail.com>
2012-10-14 02:00:06 -0600

    Add size arg to check factory

--- ui-check.lua
+++ ui-check.lua
@@ -11,10 +11,10 @@ local function CheckSound(self) PlaySound(sounds[self:GetChecked()]) end
 
 -- Creates a checkbox.
 -- All args optional but parent is highly recommended
-function ns.NewCheckBox(parent, ...)
+function ns.NewCheckBox(parent, size, ...)
 	local check = CreateFrame("CheckButton", nil, parent)
-	check:SetWidth(26)
-	check:SetHeight(26)
+	check:SetWidth(size or 26)
+	check:SetHeight(size or 26)
 	if select(1, ...) then check:SetPoint(...) end
 
 	check:SetNormalTexture("Interface\\Buttons\\UI-CheckBox-Up")
