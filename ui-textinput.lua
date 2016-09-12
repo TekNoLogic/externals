3a95e0bdf611fba8d55c030d500c000ccff2d267
Tekkub <tekkub@users.noreply.github.com>
2016-09-11 19:50:35 -0600

    Update ui-textinput.lua

--- ui-textinput.lua
+++ ui-textinput.lua
@@ -5,11 +5,22 @@ local myname, ns = ...
 local BORDER_TEXTURE = "Interface\\Common\\Common-Input-Border"
 
 
+local function OnEditFocusGained(self)
+  self.placeholder:Hide()
+end
+
+
+local function ShowPlaceholderIfEmpty(self)
+	if self:GetText() == "" then self.placeholder:Show() end
+end
+
+
+local FONT = "GameFontHighlightSmall"
 function ns.NewTextInput(parent)
   local editbox = CreateFrame("EditBox", nil, parent)
   editbox:SetAutoFocus(false)
   editbox:SetSize(105, 32)
-  editbox:SetFontObject("GameFontHighlightSmall")
+  editbox:SetFontObject(FONT)
 
   local left = editbox:CreateTexture(nil, "BACKGROUND")
   left:SetSize(8, 20)
@@ -30,6 +41,15 @@ function ns.NewTextInput(parent)
   center:SetTexture(BORDER_TEXTURE)
   center:SetTexCoord(0.0625, 0.9375, 0, 0.625)
 
+  local placeholder = editbox:CreateFontString(nil, nil, FONT)
+  placeholder:SetPoint("LEFT")
+  placeholder:SetTextColor(0.75, 0.75, 0.75, 1)
+  editbox.placeholder = placeholder
+
+  editbox:HookScript("OnEditFocusGained", OnEditFocusGained)
+  editbox:HookScript("OnEditFocusLost", ShowPlaceholderIfEmpty)
+  editbox:HookScript("OnTextSet", ShowPlaceholderIfEmpty)
+  
   editbox:SetScript("OnEscapePressed", editbox.ClearFocus)
   editbox:SetScript("OnEnterPressed", editbox.ClearFocus)
 
